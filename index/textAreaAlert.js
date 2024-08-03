c5ce7efe940aacaeda6af9ea169ea2b0cb8166e6
Roluna <73280987+Roluna1@users.noreply.github.com>
2024-08-03 23:38:23 +0800

    Delete index directory

--- index/textAreaAlert.js
+++
@@ -1,6 +0,0 @@
-export function autoAdjustHeight() {
-  const textarea = document.querySelector('.text-area-alert');
-  textarea.style.height = 'auto'; // Reset height to auto
-  textarea.style.height = textarea.scrollHeight + 'px'; // Set height to content height
-  document.querySelector('.text-area-alert').addEventListener('input', autoAdjustHeight);
-}
